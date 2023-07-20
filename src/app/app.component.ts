a1005ec3eccc732643c6a2a142daab6b97609032
Douglas M <rdm.douglas@hotmail.com>
2023-07-20 19:22:21 -0300

    feat: implement navigation component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { INavigation } from './navigation/navigation.model';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-    title = 'pm-challenge';
+export class AppComponent implements OnInit {
+    items!: INavigation[];
+
+    ngOnInit(): void {
+        this.items = [
+            { name: '1', description: 'Sales Pipeline Settings', selected: true },
+            { name: '2', description: 'Opportunity Details Settings' },
+        ]
+    }
+
+    onNext(): void {
+        console.log('next');
+    }
 }
---
+++ src/app/navigation/navigation.model.ts
@@ -0,0 +1,5 @@
+export interface INavigation {
+    name: string;
+    description: string;
+    selected?: boolean;
+}
